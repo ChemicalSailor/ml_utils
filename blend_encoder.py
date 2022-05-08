4141a68722da8c317174f47fcd3e0386e2de4173
ChemicalSailor <tf.hartle@gmail.com>
2022-05-08 14:07:01 +0100

    Made quantity parsing optional in BlendEncoder. Added class documentation to BlendEncoder.

--- blend_encoder.py
+++ blend_encoder.py
@@ -7,25 +7,29 @@ Modified one-hot encoder for blends.
 # TODO: Test missing sample
 #   check_array checks for missing values (np.nan or pd.NA) provided input is 'object' dtype
 #   need to decide if input should be cast, in which case there should be string replacement for empty values
-# TODO: extent class documentation with description and examples
 # TODO: handle unknown solvents passed to transform
 #   currently implicitedly converted to 'none' and dropped meaning validation fails
 
 import numpy as np
-import pandas as pd
 from sklearn.base import TransformerMixin, BaseEstimator
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.utils.validation import check_is_fitted, check_array, _check_feature_names_in
 import re
-from typing import List, Tuple, Iterable
+from typing import List, Tuple, Iterable, Optional, Union
 from numpy.typing import ArrayLike, NDArray
 
 
 class _BlendParser:
 
-    def __init__( self, delimit: str = ':', case_sensitive: bool = False ):
+    def __init__(
+            self,
+            delimit: str = ':',
+            case_sensitive: bool = False,
+            parse_quantities: bool = True
+            ):
         self.delimit = delimit
         self.case_sensitive = case_sensitive
+        self.parse_quantities = parse_quantities
 
     def _get_regex( self ):
         """Compile regex for given state."""
@@ -34,7 +38,7 @@ class _BlendParser:
         regex = re.compile( regex_str, flag )
         return regex
 
-    def __call__( self, value: str ) -> Tuple[ List[str], List[float] ]:
+    def __call__( self, value: str ) -> Union[ Tuple[ List[str], List[float] ], List[str] ]:
         """
         Parse a string with blend information.
 
@@ -47,27 +51,56 @@ class _BlendParser:
         -------
         Tuple[ List[str], List[float] ]
             A list of found components and a list of matching quantities.
+        List[str]
+            A list of found components when `parse_quantities=False`.
 
         Raises
         ------
         ValueError
-            When the length of found components and found quantities do not match.
+            When the given string does not match the expected components
+            quantities pattern.
+        ValueError
+            When the length of found components and found quantities do not
+            match.
 
         """
-        return self._parse( self._get_regex(), value )
+        c, q = self._parse( self._get_regex(), value )
+        if self.parse_quantities:
+            return c, q
+        return c
 
-    def _parse( self, regex, value: str ) -> Tuple[ List[str], List[float] ]:
+    def _parse( self, regex, value: str ) -> Tuple[ List[str], Optional[List[float]] ]:
         """Parse string with pre-compiled regex."""
         # handle the pure solvent case first
         if self.delimit not in value:
             # assume that the whole string is the solvent name
-            return [value], [1.0]
+            if self.parse_quantities:
+                return [value], [1.0]
+            return [value], None
 
-        components, quantities = regex.match( value ).groups()
+        # try to match a complete string and fall back to the partial string
+        # if not matching quantities
+        try:
+            components, quantities = regex.match( value ).groups()
+        except AttributeError:
+            if self.parse_quantities:
+                raise ValueError(
+                    "Could not match components and quantities pattern in the "
+                    "string `{}`; validate string format or use "
+                    "`parse_quantities=False`". format( value )
+                    )
+            components = value
+
         components = components.split( self.delimit )
+
+        if not self.parse_quantities:
+            return components, None
+
         quantities = quantities.split( self.delimit )
 
-        if ( n_solv := len(components) ) != ( n_quan := len(quantities) ):
+        n_solv = len(components)
+        n_quan = len(quantities)
+        if n_solv != n_quan:
             raise ValueError(
                 "Mismatch in solvent blend string '{}', found {} components "
                 "and {} quantities".format( value, n_solv, n_quan)
@@ -87,10 +120,10 @@ class _BlendParser:
         quantities = []
         for x_ in x:
             _c, _q = self._parse( regex, x_ )
-            components.append( _c )
             quantities.append( _q )
-            if ( n := len( _c ) ) > max_components:
-                max_components = n
+            components.append( _c )
+            if len( _c ) > max_components:
+                max_components = len( _c )
 
         def _cast_array( values, empty_val, dtype ) -> NDArray:
             arr = np.full(
@@ -104,8 +137,11 @@ class _BlendParser:
             return arr
 
         components = _cast_array( components, 'none', 'O' )
-        quantities = _cast_array( quantities, 0, 'float' )
 
+        if not self.parse_quantities:
+            return components
+
+        quantities = _cast_array( quantities, 0, 'float' )
         # normalise quantities
         quantities = quantities / quantities.sum( axis=1, keepdims=True )
 
@@ -131,7 +167,25 @@ class _BlendParser:
 
 class BlendEncoder( BaseEstimator, TransformerMixin ):
     """
-    Encodes strings describing a blend or mixture.
+    Encodes strings describing mixtures of arbitary components.
+
+    A blend describes a mixture of two or more components in specific quantities.
+    A binary mixture of components is represented as a string in the format
+    `'A:B x:y'` where A and B are the two component names and x:y give the ratio
+    of the respective components; higher order mixtures are similarly
+    represented by extending the delimiter pattern e.g. `'A:B:C x:y:z'` for a
+    ternary mixture.
+
+    Naively encoding each unique blend as a category discards potentially
+    important relational information; two blends `'A:B 8:2'` and `'A:B 9:1'` may
+    have similar properties whereas `'C:D 8:2'` could be entirely different.
+    Blends with varying component types should not be encoded ordinally and a
+    one-hot encoding could lead to a large increase in the number of features.
+
+    This encoder parses each string to extract each component name and
+    optionally it's quantity information. A one-hot encoding is created based on
+    the unique collection of components found with the resultant vectors scaled
+    to represent the component ratios.
 
     Parameters
     ----------
@@ -140,10 +194,11 @@ class BlendEncoder( BaseEstimator, TransformerMixin ):
     case_sensitive : bool, default = False
         Perform case sensitive matching or not; by default matching is
         case insensitive.
-    sparse : bool, default = False
-        Return a sparse matrix.
+    parse_quantities : bool, deafult = True
+        Extract quantity information and scale vectors accordingly.
     validate : bool, default = True
-        Check that blend quantities sum to 100%.
+        Check that blend quantities sum to 100%. Only relevent when
+        `parse_quantities=True`.
 
     Attributes
     ----------
@@ -157,18 +212,68 @@ class BlendEncoder( BaseEstimator, TransformerMixin ):
     components_ : list of length `n_features_in_`
         The found component names (as an array) for each feature.
 
+    Examples
+    --------
+    Given a dataset of arbitary blends the encoder finds the unique components
+    and quantities:
+
+    >>> X = np.array([['A:B 1:1'], ['A:C 25.5:74.5'], ['B:C:A 1:1:2'], ['C']])
+    >>> enc = BlendEncoder().fit(X)
+    >>> enc.components_
+    [array(['A', 'B', 'C'], dtype=object)]
+    >>> enc.transform(X)
+    array([[0.5  , 0.5  , 0.   ],
+          [0.255, 0.   , 0.745],
+          [0.5  , 0.25 , 0.25 ],
+          [0.   , 0.   , 1.   ]])
+
+    Feature names are generated as per scikit-learns's OneHotEncoder:
+
+    >>> enc.get_feature_names_out()
+    ['x0_A', 'x0_B', 'x0_C']
+
+    Component ratios can be ignored, in which case a one-hot encoding of the
+    unique components is given:
+
+    >>> enc = BlendEncoder(parse_quantities=False)
+    >>> enc.fit_transform(X)
+    array([[1., 1., 0.],
+          [1., 0., 1.],
+          [1., 1., 1.],
+          [0., 0., 1.]])
+
+    Blends without component ratios are similarly supported:
+
+    >>> X = np.array([['A:B'], ['A:C:D'], ['B'], ['B:D'], ['A:D']])
+    >>> enc = BlendEncoder(parse_quantities=False)
+    >>> enc.fit_transform(X)
+    array([[1., 1., 0., 0.],
+          [1., 0., 1., 1.],
+          [0., 1., 0., 0.],
+          [0., 1., 0., 1.],
+          [1., 0., 0., 1.]])
+
+    Notes
+    -----
+    The regex for matching components matches any word-type character plus
+    characters from the set `-()[]` at the start of the string. Component
+    quantities are expected as integer or decimal numbers at the end of the
+    string. Components and quantities must be separated by whitespace. If no
+    delimiter is present in the string it is assumed to be a single component
+    blend.
+
     """
 
     def __init__(
             self, *,
             delimit: str = ':',
             case_sensitive: bool = False,
-            sparse: bool = False,
+            parse_quantities: bool = True,
             validate: bool = True,
             ):
         self.delimit = delimit
         self.case_sensitive = case_sensitive
-        self.sparse = sparse
+        self.parse_quantities = parse_quantities
         self.validate = validate
 
     def _check_X( self, X: ArrayLike, *, reset=False ):
@@ -184,6 +289,7 @@ class BlendEncoder( BaseEstimator, TransformerMixin ):
         return np.array( X )
 
     def _fit_transform( self, X: ArrayLike, transform: bool ):
+        # X = np.atleast_2d( X )
         X = self._check_X( X, reset=True )
         encoders = []
         components = []
@@ -353,8 +459,17 @@ class BlendEncoder( BaseEstimator, TransformerMixin ):
         return X
 
     def _parse_1d( self, x ):
-        parser = _BlendParser( self.delimit, self.case_sensitive )
-        return parser.map( x )
+        parser = _BlendParser(
+            self.delimit,
+            case_sensitive=self.case_sensitive,
+            parse_quantities=self.parse_quantities
+            )
+        if self.parse_quantities:
+            return parser.map( x )
+        # construct a dummy array of quantities
+        _components = parser.map( x )
+        _quantities = np.ones_like( _components, dtype=np.float64 )
+        return _components, _quantities
 
     def _fit_1d( self, components ):
         categories = np.unique( components.flatten() )
@@ -363,7 +478,7 @@ class BlendEncoder( BaseEstimator, TransformerMixin ):
 
         one_hot = OneHotEncoder(
             categories=[categories]*max_components,
-            sparse=self.sparse,
+            sparse=False,
             handle_unknown='ignore',  # drops out 'none' entries
             )
         one_hot.fit( components )
@@ -371,6 +486,10 @@ class BlendEncoder( BaseEstimator, TransformerMixin ):
 
     def _transform_1d( self, components, quantities, encoder ):
         n_categories = len( encoder.categories_[0] )
+        n_features = encoder.n_features_in_
+        if components.shape[1] < n_features:
+            components = np.pad( components, ((0, 0), (0, 1)), constant_values='none' )
+            quantities = np.pad( quantities, ((0, 0), (0, 1)), constant_values=0 )
         encoded_2d = encoder.transform( components )
 
         _zip = zip(
@@ -381,7 +500,7 @@ class BlendEncoder( BaseEstimator, TransformerMixin ):
             [ q.reshape(-1, 1) * encoded_2d[:, i:i+n_categories] for i, q in _zip ]
             ).sum( axis=0 )
 
-        if self.validate and not self._validate_mixture( encoded_stack ):
+        if self.parse_quantities and self.validate and not self._validate_mixture( encoded_stack ):
             raise ValueError( 'Found one or more blends that do not sum to 100%' )
 
         return encoded_stack
@@ -393,8 +512,8 @@ class BlendEncoder( BaseEstimator, TransformerMixin ):
 
     def _inverse_transform_1d( self, X, categories ):
         m, n = X.shape
-        if n != ( n_cat := len( categories ) ):
-            raise ValueError( f"Shape {X.shape} of `X` did not match the given number of categories {n_cat}" )
+        if n != len( categories ):
+            raise ValueError( f"Shape {X.shape} of `X` did not match the given number of categories {len( categories )}" )
 
         gen = ( [ (name, val * 100) for name, val in zip( categories, x ) if val ] for x in X )
         return np.array( list( self._join( gen ) ) ).reshape( -1, 1 )
